801a969e1966af69346f107204a2fa999a666c0e
Saurabh Kushwaha <saurabhkushwaha4201@gmail.com>
2025-11-11 16:56:58 +0530

    Add new React project

---
+++ 01ViteProject/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Suru from './suru.jsx'
+
+const App = () => {
+  return (
+    <>
+      <h1>My name is Saurabh</h1>
+      <Suru/>
+    </>
+  )
+}
+
+export default App
---
+++ 01ViteProject/src/suru.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Suru = () => {
+  return (
+    <>
+    <h2>I am from Bihar</h2>
+    </>
+  )
+}
+
+export default Suru
